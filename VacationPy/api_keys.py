2fc1dd541f8516c16b1f61e385ca74223893556a
supraja2710 <kvsupraja2710@gmail.com>
2021-05-07 04:45:14 -0400

    Done

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,2 @@
+# Google API Key
+g_key = "AIzaSyAJC6CU7i-v4drBB3zR630wB6FgxucUFf8"
--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,2 @@
 # OpenWeatherMap API Key
 weather_api_key = "fe9dcfd6b3306e2c6ea9f778d1ae8d14"
-
-# Google API Key
-g_key = "YOUR KEY HERE!"
